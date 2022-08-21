ca7f6c3def65e0739e7fb4e4c38022926764b395
Chintan Patel <patelchintan843@gmail.com>
2022-08-21 23:27:38 +0530

    add exercise inheritance file

--- Inheritance/ExerciseInheritance.cpp
+++ Inheritance/ExerciseInheritance.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 class SimpleCalculator
@@ -6,7 +7,7 @@ class SimpleCalculator
 protected:
     int num1;
     int num2;
-    char choice;
+    char choice1;
 
 public:
     void setNumber(int n1, int n2)
@@ -17,11 +18,11 @@ public:
     void setChoice()
     {
         cout << "Enter the choice which you want to perform: " << endl;
-        cin >> choice;
+        cin >> choice1;
     }
-    void performOperation()
+    void performOperationSimple()
     {
-        switch (choice)
+        switch (choice1)
         {
         case '+':
             cout << "Two number sum is: " << num1 + num2 << endl;
@@ -37,7 +38,60 @@ public:
             break;
         default:
             cout << "Please enter the valid choice..!!" << endl;
+            break;
         }
+    }
+};
+class ScientificCalculator
+{
+protected:
+    int number1;
+    int number2;
+    char choice2;
+
+public:
+    void setNumber(int n1, int n2)
+    {
+        number1 = n1;
+        number2 = n2;
+    }
+    void setChoice()
+    {
+        cout << "Enter the choice which you want to perform: " << endl;
+        cin >> choice2;
+    }
+    void performOperationScientific()
+    {
+        switch (choice2)
+        {
+        case 'r':
+            cout << "Remainder is: " << number1 % number2 << endl;
+            break;
+        case 'p':
+            cout << "Power of number1 is: " << number1 * number1 << endl;
+            cout << "Power of number2 is: " << number2 * number2 << endl;
+            break;
+        case 's':
+            cout << "Power of number1 is: " << float(sqrt(number1)) << endl;
+            cout << "Power of number2 is: " << float(sqrt(number2)) << endl;
+            break;
+        case 'c':
+            cout << "Cube of number1 is: " << pow(number1, 3) << endl;
+            cout << "Cube of number2 is: " << pow(number2, 3) << endl;
+            break;
+        default:
+            cout << "Please enter the valid choice..!!" << endl;
+            break;
+        }
+    }
+};
+
+class HybridCalculator : public SimpleCalculator, public ScientificCalculator{
+public:
+    
+    void display() {
+        cout << "Your simple calculation result is: " <<  performOperationSimple() << endl;
+        cout << "Your scientific calculation result is: " <<  performOperationScientific() << endl;
     }
 };
 
@@ -46,6 +100,11 @@ int main()
     SimpleCalculator s1;
     s1.setNumber(12, 23);
     s1.setChoice();
-    s1.performOperation();
+    s1.performOperationSimple();
+
+    ScientificCalculator s2;
+    s2.setNumber(32, 16);
+    s2.setChoice();
+    s2.performOperationScientific();
     return 0;
 }
